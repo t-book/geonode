175bef113c31c592fc58093545b3653ff7e3ac8c
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2024-01-21 10:50:16 +0100

    Sanitize HTML data

---
+++ geonode/base/templatetags/sanitize_html.py
@@ -0,0 +1,32 @@
+from django import template
+from django.conf import settings
+import nh3
+
+register = template.Library()
+
+
+@register.filter(name="sanitize_html")
+def sanitize_html(value):
+    """
+    This filter can be used in Django templates to ensure that HTML content is safe to render. It leverages the
+    nh3 library (https://github.com/messense/nh3) to remove dangerous tags and JavaScript patterns.
+
+    The allowed HTML tags and attributes can be
+    configured via the 'NH3_DEFAULT_CONFIG' setting in Django's settings.py. If this configuration is not set, a default
+    set of safe tags will be used.
+
+    Usage in templates: {{ some_variable|sanitize_html|safe }}
+
+    :param value: The HTML content to be sanitized within the template.
+    :return: Sanitized HTML content safe for rendering in templates.
+    """
+
+    nh3_config = getattr(
+        settings,
+        "NH3_DEFAULT_CONFIG",
+        {
+            "tags": {"b", "a", "img", "p", "ul", "li", "strong", "em", "span"},
+        },
+    )
+
+    return nh3.clean(value, **nh3_config)
